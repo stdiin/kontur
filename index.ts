034c09c34cd435d699a9424d43a14588cb55d7f9
foxi35338 <foxi35338@gmail.com>
2026-06-22 14:04:48 +0200

    Replaced css background with canvas image

--- index.ts
+++ index.ts
@@ -1,24 +1,32 @@
 let isPlaying = false
 
-const game = document.getElementById("game")
-const mainMenu = document.getElementById("main-menu")
+const game = document.getElementById("game") as HTMLDivElement
+const mainMenu = document.getElementById("main-menu") as HTMLDivElement
+const map = document.getElementById("map") as HTMLCanvasElement
+const ctx = map.getContext("2d")!
+
+function drawMap(img: HTMLImageElement) {
+   img.onload = () => {
+      map.width = map.offsetWidth
+      map.height = map.offsetHeight
+      ctx.imageSmoothingQuality = "high"
+      ctx.drawImage(img, window.innerWidth / 2 - img.width / 2, window.innerHeight / 2 - img.height / 2)
+   }
+}
 
 function startGame() {
-   if (!game || !mainMenu) {
-      return
-   }
+   const img = new Image()
+   img.src = "assets/north-america-map.png"
 
-   console.log("game started")
+   drawMap(img)
 
    game.classList.remove("hidden")
    mainMenu.classList.add("hidden")
-
    isPlaying = true
 }
 
 function endGame() {
-   console.log("game ended")
-   game?.classList.add("hidden")
-   mainMenu?.classList.remove("hidden")
+   game.classList.add("hidden")
+   mainMenu.classList.remove("hidden")
    isPlaying = false
 }
